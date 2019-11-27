359f12f7d24728a93c012c8565ae4c1ab69b0c72
HaRyoung <qsss@naver.com>
2019-11-28 02:45:53 +0900

    update something

--- gotrip/views.py
+++ gotrip/views.py
@@ -19,9 +19,17 @@ def generic(request, city_name):
 
 def gallery(request, festa_id):
     festa_detail = get_object_or_404(Festa, pk = festa_id)
-    #instas = Insta.objects.filter(tag = festa_detail.id)
-    result1 = Insta.objects.all()[:3]
-    result2 = Insta.objects.all()[3:6]
-    result3 = Insta.objects.all()[6:9]
+    instas = Insta.objects.filter(tag = festa_detail.id)
+    result1 = [];
+    result2 = [];
+    result3 = [];
+    for i in range(0,3) :
+        result1.append(instas[i]);
+
+    for i in range(3,6) :
+        result2.append(instas[i]);
+
+    for i in range(6,9) :
+        result3.append(instas[i]);
     
     return render(request, 'gallery.html', {'festa': festa_detail , 'instas1' : result1, 'instas2' : result2, 'instas3' : result3})
